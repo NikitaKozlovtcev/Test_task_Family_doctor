1b0d4c0b9af0f352a404a09eb21f45da4a1167ac
Nikita Kozlovtsev <Nikita_Kozlovtsev@dietsmann.com>
2022-02-10 17:04:29 +0300

    Updated doc string

--- src/app/api/weather.py
+++ src/app/api/weather.py
@@ -2,19 +2,26 @@ import logging
 from datetime import datetime
 from typing import Optional
 
-from fastapi import APIRouter, HTTPException, Query
-
 from app.api import crud_weather
 from app.api.open_weather_api import get
+from fastapi import APIRouter, HTTPException, Query
 
 router = APIRouter()
 
 log = logging.getLogger(__name__)
 
+
 @router.get("/weather", name='Get wheather by city with region and time')
 async def main_get_weather(country_code: Optional[str] = Query(..., min_length=2, max_length=3),
                            city: Optional[str] = Query(..., min_length=3, max_length=50),
                            date: Optional[str] = Query(..., min_length=2, max_length=50)):
+    """
+        - Реализовать GET API /weather, которое в качестве параметров принимает код страны, город и дату со временем.
+        - Сервис должен ответить информацией о погоде, воспользовавшись api OpenWeather (https://openweathermap.org/)
+        - Например: /weather?country_code=RU&city=Moscow&date=< +/-5 дней >T12:00
+        - Выдает информацию о погоде для Москвы на 12:00 выбранного дня
+        - Полученную информацию сохранять и при повторном запросе уже возвращать, не обращаясь к api сервиса погоды.
+    """
     try:
         date = datetime.fromisoformat(date)
         timestamp = str(int(datetime.timestamp(date)))
